72a80603d8cea6b52ef5e13d9f5b7d2f21f0722c
Jakub Jerabek <jakub.jerabek@growjob.com>
2024-08-19 14:58:02 +0200

    fighting + tests

--- game_functions.py
+++ game_functions.py
@@ -1,13 +1,15 @@
 from colorama import Fore, Back, Style
 import random
+import sys
+import time
 """
 Knihovny: unittest, coloroma
-
-
 """
 
+
 def create_character():
-    # Vytvoření proměnných pro jméno postavy, zdraví, sílu a inventář
+    # Vytvoření proměnných pro jméno postavy, zdraví, peníze a inventář
+
     health = 100  # Číslo
     coins = 0  # Číslo
     inventory = []  # Pole/Seznam/List
@@ -20,7 +22,14 @@ def create_character():
         "inventory": inventory
     }
 
-def get_character_name():
+def get_character_name() -> str:
+    """
+    Zeptá se hráče, jaké chce mít jméno charakteru a pozdraví ho
+
+    Returns:
+        str: Jméno charakteru zadané uživatelem
+    """
+
     character_name = input("Zadej jméno: ")
     
     if len(character_name) == 0:
@@ -30,10 +39,30 @@ def get_character_name():
     print(f"Ahoj, {character_name}, tvé dobrodružství začíná...")
     return character_name
 
-def get_player_choice():
+def get_player_choice(character) -> str:
+    """
+    Zeptá se hráče, jakou chce provést akci a vypíše základní informace o charakteru
+
+    Parameters:
+    character (dict): Slovník obsahující informace o postavě, včetně mincí a inventáře.
+    """
+
+    print("-----------------------------------------------------------------------")
+    print(Fore.RED + f"Životy: {character["health"]}" )
+    print(Fore.BLUE + f"Peníze: {character["coins"]}" )
+    print("-----------------------------------------------------------------------")
+
     return input("Co chceš dělat? (např. 'prozkoumat', 'mluvit', 'bojovat', 'obchodovat', 'quit'): ")
 
 def process_action(action, character):
+    """
+    Zavolá odpovídající funkci dle typu akce
+
+    Parameters:
+    action (str): Řetězec obsahující název akce
+    character (dict): Slovník obsahující informace o postavě, včetně mincí a inventáře.
+    """
+
     if action == 'prozkoumat':
         explore(character)
     elif action == 'obchodovat':
@@ -46,15 +75,19 @@ def process_action(action, character):
         print("Neznámá akce.")
 
 def explore(character):
+    print("Prozkoumáváš okolí...")
+
+    time.sleep(2)
+
     discoveries = ["starý meč", "zlatá mince", "magický amulet", "nic"]
 
     found_item = random.choice(discoveries)
     
     if found_item != "nic":
         character['inventory'].append(found_item)
-        print(f"Našel jsi {found_item}!")
+        print(Fore.GREEN + f"Našel jsi {found_item}!")
     else:
-        print("Prozkoumal jsi oblast, ale nenašel jsi nic zajímavého.")
+        print(Fore.LIGHTBLACK_EX + "Prozkoumal jsi oblast, ale nenašel jsi nic zajímavého.")
 
 def trade(character):
     """
@@ -70,21 +103,22 @@ def trade(character):
     ]
     
     # Zobrazení dostupných předmětů a jejich cen
-    print("Dostupné předměty k nákupu:")
+    print(Fore.LIGHTMAGENTA_EX +  "Dostupné předměty k nákupu:")
     for index, item in enumerate(items_for_sale):
         print(f"({index + 1}) {item['name']} - Cena: {item['price']} mincí")
     
     # Získání uživatelského vstupu
     product_index = int(input("Co chceš nakoupit? (Vyber číslo): ")) - 1
+    print(f"product_index: {product_index}")
     if product_index < 0 or product_index >= len(items_for_sale):
-        print("Neplatný výběr.")
+        print(Fore.RED +  "Neplatný výběr.")
         return
 
     product = items_for_sale[product_index]
 
     # Kontrola, zda má hráč dost mincí
     if character['coins'] < product['price']:
-        print("Nemáš dost mincí na koupi tohoto předmětu.")
+        print(Fore.RED + "Nemáš dost mincí na koupi tohoto předmětu.")
         return
 
     # Provedení nákupu
@@ -97,19 +131,57 @@ def talk(character):
     print("Mluvíš s postavou.")
 
 def fight(character):
-    print("Bojuješ.")
+    """
+    Boj s příšerou na základě hádání čísla od 0 po 100.
+    Příšera hadá taky. Ten kdo uhodne blíž, vyhrává.
+
+    Parameters:
+    character (dict): Slovník obsahující informace o postavě, včetně mincí a inventáře.
+    """
+
+    fight_number = random.randrange(0,100)
+
+    enemy_number = random.randrange(0,100)
+    player_number = int(input("Hádej číslo v rozsahu 0 až 100. Pokud se trefíš blíž než nepřítel, vyhraješ!"))
+
+    if player_number <= 0 or player_number >= 100:
+        print(Fore.RED + "Zadáno neplatné číslo, vrácíme se zpět!")
+        return
+
+    enemy_difference = abs(fight_number - enemy_number)
+    player_difference = abs(fight_number - player_number)
+
+    print(f"Bojuješ s nepřítelem! Hádané číslo: {fight_number}")
+
+    time.sleep(2)
+
+    if player_difference < enemy_difference:
+        print(Fore.GREEN + "Zvítězil jsi nad nepřítelem a získal jsi 100 peněz!")
+        character['inventory'].append('poklad')
+        character['coins'] += 100
+    elif player_difference == enemy_difference:
+        print(Fore.LIGHTBLACK_EX + "Boj byl nerozhodný, oba ustupujete.")
+    else:
+        print(Fore.RED + "Nepřítel byl příliš silný. Utrpěl jsi zranění.")
+        character['health'] -= 30
+
+        check_player_dead(character)
+
+def check_player_dead(character):
+    print("Byl jsi zabit!")
+    print(Fore.BLUE + f"Počet peněz: {character["coins"]}")
+    sys.exit(0)
 
 def game_loop():
     print("Vítejte ve hře! Jak se jmenuješ?")
 
     character = create_character()
 
-    print(f"Ahoj, {character["name"]}, tvé dobrodružství začíná...")
     # Vypíšeme informace o charakteru:
     print(Fore.BLUE + f"Jméno: {character["name"]}, Životů: {character["health"]}, Peněz: {character["coins"]}, Počet itemů v inventáři: {len(character["inventory"])}")
 
     while True:
-        action = get_player_choice()
+        action = get_player_choice(character)
         if action.lower() == 'quit':
             break
         process_action(action, character)
--- test.py
+++ test.py
@@ -5,6 +5,7 @@ import unittest
 from unittest.mock import patch
 from game_functions import *
 
+
 class TestCharacterCreation(unittest.TestCase):
     def setUp(self):
         # Vytvoříme zkušební postavu pro použití ve všech testech
@@ -55,13 +56,42 @@ class TestCharacterCreation(unittest.TestCase):
         self.assertNotIn('Život', self.character['inventory'])
         self.assertEqual(self.character['coins'], 30)  # Mince by se neměly změnit
 
-    @patch('builtins.input', side_effect=[99])  # Neplatný výběr produktu
-    def test_invalid_input(self, mock_input):
+    @patch('builtins.input', side_effect=[98])  # Neplatný výběr produktu
+    def test_trade_invalid_input(self, mock_input):
         trade(self.character)
         self.assertTrue(len(self.character['inventory']) == 0)
         self.assertEqual(self.character['coins'], 150)  # Mince by se neměly změnit
 
+    @patch('random.randrange', side_effect=[50, 45])  # Předpokládáme, že bojové číslo je 50, nepřítel hádá 45
+    @patch('builtins.input', return_value='48')  # Hráč hádá 48
+    @patch('time.sleep', return_value=None)
+    def test_player_wins(self, mock_input, mock_randrange, _):
+        fight(self.character)
+        self.assertEqual(self.character['coins'], 250)
+        self.assertEqual(self.character['health'], 100)
 
+    @patch('random.randrange', side_effect=[50, 49])  # Nepřítel je blíže
+    @patch('builtins.input', return_value='30')
+    @patch('time.sleep', return_value=None)
+    def test_player_loses(self, mock_input, mock_randrange, _):
+        fight(self.character)
+        self.assertEqual(self.character['coins'], 150)
+        self.assertEqual(self.character['health'], 70)
+
+    @patch('random.randrange', side_effect=[50, 48])  # Nerozhodný
+    @patch('builtins.input', return_value='48')
+    @patch('time.sleep', return_value=None)
+    def test_draw(self, mock_input, mock_randrange, _):
+        fight(self.character)
+        self.assertEqual(self.character['health'], 100)
+
+    @patch('random.randrange', side_effect=[50, 60])  # Neplatný vstup, hra přečte 50
+    @patch('builtins.input', return_value='101')
+    @patch('time.sleep', return_value=None)
+    def test_fight_invalid_input(self, mock_input, mock_randrange, _):
+        fight(self.character)
+        self.assertEqual(self.character['coins'], 150)  # Zůstávají stejné
+        self.assertEqual(self.character['health'], 100)  # Žádná změna
 
 if __name__ == '__main__':
     init(autoreset=True)
